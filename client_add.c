dac754303b2342232fb72c58103cab12aac2e285
rachel648 <rachelraqelmurithi@gmail.com>
2024-03-28 17:55:44 +0300

    socket

--- client_add.c
+++ client_add.c
@@ -2,69 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 
 #define PORT 8080
+#define BUFFER_SIZE 1024
 
-int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        printf("Usage: %s <server_IP> <number1> <number2>\n", argv[0]);
-        exit(1);
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <integer1> <integer2>\n", argv[0]);
+        return 1;
     }
 
     int sockfd;
     struct sockaddr_in serv_addr;
-    char buffer[1024] = {0};
+    char buffer[BUFFER_SIZE] = {0};
 
-    // Get the server IP address from the command line argument
-    const char* server_ip = argv[1];
-
-    // Get the two integers from the command line arguments
-    int num1 = atoi(argv[2]);
-    int num2 = atoi(argv[3]);
-
-    // Create a socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        perror("socket creation failed");
-        exit(1);
+    // Create socket
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        return 1;
     }
-
-    memset(&serv_addr, 0, sizeof(serv_addr));
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    // Convert IP address string to binary form
-    if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0) {
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
         perror("Invalid address/ Address not supported");
-        exit(1);
+        return 1;
     }
 
-    // Connect to the server
-    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("connection failed");
-        exit(1);
+    // Connect to server
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection failed");
+        return 1;
     }
 
-    // Send the two integers to the server
-    sprintf(buffer, "%d %d", num1, num2);
-    send(sockfd, buffer, strlen(buffer), 0);
+    // Send integers to server
+    char send_buffer[BUFFER_SIZE];
+    sprintf(send_buffer, "%s %s", argv[1], argv[2]);
+    send(sockfd, send_buffer, strlen(send_buffer), 0);
 
-    // Receive the sum from the server
-    int bytes_received = recv(sockfd, buffer, sizeof(buffer), 0);
-    if (bytes_received == -1) {
-        perror("recv failed");
-        exit(1);
-    } else if (bytes_received == 0) {
-        printf("Server disconnected\n");
-        exit(1);
-    }
-
-    int sum = atoi(buffer);
-    printf("Sum from server: %d\n", sum);
+    // Receive result from server
+    int valread = read(sockfd, buffer, BUFFER_SIZE);
+    printf("Sum received from server: %s\n", buffer);
 
     close(sockfd);
     return 0;
--- server_add.c
+++ server_add.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
 #define PORT 8080
+#define BUFFER_SIZE 1024
 
 int main() {
-    int server_fd, new_socket, valread;
+    int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[1024] = {0};
+    char buffer[BUFFER_SIZE] = {0};
 
-    // Create a socket file
+    // Create socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
+        perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
@@ -25,20 +25,43 @@ int main() {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
-
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Bind the socket to the address
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        perror("bind failed");
+    // Bind the socket to localhost port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
+    // Listen for incoming connections
     if (listen(server_fd, 3) < 0) {
-        perror("listen");
+        perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
-    printf("Server listening on port %d
+    while (1) {
+        // Accept a new connection
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+            perror("Accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Read integers from client
+        int num1, num2;
+        read(new_socket, buffer, BUFFER_SIZE);
+        sscanf(buffer, "%d %d", &num1, &num2);
+
+        // Calculate sum
+        int sum = num1 + num2;
+
+        // Send sum back to client
+        char sum_str[BUFFER_SIZE];
+        sprintf(sum_str, "%d", sum);
+        send(new_socket, sum_str, strlen(sum_str), 0);
+
+        close(new_socket);
+    }
+    return 0;
+}
